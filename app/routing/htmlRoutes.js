af12815745f5ecd9f370699efad431face590ebd
imalam64 <imalam64@gmail.com>
2018-08-26 14:42:20 -0700

    created HTML routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,15 @@
+var path = require('path');
+
 //This should contain two routes:
 
+module.exports = function(app) {
 //1. A GET Route to /survey which should display the survey page.
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
 
 //2. A default, catch-all route that leads to home.html which displays the home page.
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 var express = require('express');
 var parse = require('body-parser');
-var path = require('path');
 
+var app = express();
+var PORT =  process.env.PORT || 8080;
+
+app.use(parse.urlencoded({ extended: true }));
+app.use(parse.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+  
